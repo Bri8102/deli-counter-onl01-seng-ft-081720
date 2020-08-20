f2590fb2fa451340b3a9501004d6c089c4b7d607
Briana Oniru <damioniru16@gmail.com>
2020-08-20 03:15:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli,name)
-  katz_deli.each_with_index(1) do |name, i|
-  #  puts "The line is currently: #{i+1}.  #{name}"
+  katz_deli.each_with_index do |name, i|
+    puts "The line is currently: #{i+1}.  #{name}"
  end
