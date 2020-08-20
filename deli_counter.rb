ade68caa33dfda2ffaf66ff046f3a9fb3996257a
Briana Oniru <damioniru16@gmail.com>
2020-08-20 03:19:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else katz_deli.each_with_index do |name, i|
+    puts "The line is currently: #{i+1}.  #{name}"
   end
 end
 
